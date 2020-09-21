6aefdc2e0dc7dba9d819f6d7b5d5266e66803738
hanzo89757 <hanzo89757@gmail.com>
2020-09-21 17:17:56 +0800

    add python virtualenv

--- zsh/yy.sh
+++ zsh/yy.sh
@@ -5,3 +5,8 @@ export KRB5KEYTAB=/usr/local/hadoop3/etc/hadoop/reco_hago.keytab
 
 # Parse log
 alias dg_parse="python $HOME/utils/parselog.v6.py comment,like,vv,follow,dr,click auc eval"
+
+# Python virtualenv
+if [ -f "$HOME/venv/bin/activate" ]; then
+    . "$HOME/venv/bin/activate"
+fi
